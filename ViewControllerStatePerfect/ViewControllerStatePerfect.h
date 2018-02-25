5bb9ee36a7fc709a0869cd1d6f680e3547723368
Yuya Hirayama <y@yura.me>
2018-02-25 11:53:44 +0900

    Add Xcode project

---
+++ ViewControllerStatePerfect/ViewControllerStatePerfect.h
@@ -0,0 +1,19 @@
+//
+//  ViewControllerStatePerfect.h
+//  ViewControllerStatePerfect
+//
+//  Created by Yuya Hirayama on 2018/02/25.
+//  Copyright © 2018年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewControllerStatePerfect.
+FOUNDATION_EXPORT double ViewControllerStatePerfectVersionNumber;
+
+//! Project version string for ViewControllerStatePerfect.
+FOUNDATION_EXPORT const unsigned char ViewControllerStatePerfectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewControllerStatePerfect/PublicHeader.h>
+
+
